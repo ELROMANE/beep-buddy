459eb73ece18ed518f8bcaa48236baf35d50524c
Piyush <justcoding.living@gmail.com>
2025-09-13 15:19:55 -0400

    sending requests and getting a response

--- cerebras_api.py
+++ cerebras_api.py
@@ -5,4 +5,20 @@ import os
 
 API_KEY = os.getenv("API_KEY")
 API_URL = "https://api.cerebras.ai/v2/chat/completions"
-print(API_URL)
+
+
+def ask_prompt (prompt):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}"
+    }
+    data = {
+        "model": "Llama 4 Scout",
+        "messages": [{"role": "user", "content": prompt}]
+    }
+
+    response = requests.post(API_URL, headers=headers, json=data)
+    if (response.status_code != 200):
+        raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
+    return response.json()['choices'][0]['message']['content']
+
+
