126f4b54a9512d26371b71b8ef60a19e252b73b2
Piyush <justcoding.living@gmail.com>
2025-09-13 19:40:44 -0400

    call out the player if there's no input and starts to listen again

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # the main code for R2D2
 
+from cerebras_api import ask_prompt
+from speech_utils import listen_here, speak_here, play_sound
+import movement_sim as r2
+
+
+while True:
+    user_input = listen_here()
+    if not user_input:
+        print("speak something motherfucking fucker")
+        user_input = listen_here()
