620ee1ec6a91eb7916b37ce2b5d880d98346ebb3
Piyush <justcoding.living@gmail.com>
2025-09-13 21:51:59 -0400

    take the url and key from the env file

--- cerebras_api.py
+++ cerebras_api.py
@@ -2,23 +2,28 @@
 
 import requests
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 API_KEY = os.getenv("API_KEY")
-API_URL = "https://api.cerebras.ai/v2/chat/completions"
+API_URL = os.getenv("API_URL")
 
 
 def ask_prompt (prompt):
     headers = {
-        "Authorization": f"Bearer {API_KEY}"
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
     }
     data = {
-        "model": "Llama 4 Scout",
+        "model": "llama-4-scout-17b-16e-instruct",
         "messages": [{"role": "user", "content": prompt}]
     }
 
     response = requests.post(API_URL, headers=headers, json=data)
-    if (response.status_code != 200):
-        raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
-    return response.json()['choices'][0]['message']['content']
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        print(f"Request failed with status code {response.status_code}: {response.text}")
+        return f"Error: {response.status_code} - {response.text}"
 
 
